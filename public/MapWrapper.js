a7c13a410431ec9c680d9ff43acbc013bf301e70
Kelsie Braidwood <kelsieiona@gmail.com>
2018-06-12 19:52:33 +0100

    adds pop up with wikilink

--- public/MapWrapper.js
+++ public/MapWrapper.js
@@ -12,6 +12,8 @@ const MapWrapper = function(element, coords, zoom){
 
 MapWrapper.prototype.moveTo = function(place){
     this.map.flyTo(place);
+    L.marker(place).addTo(this.map).bindPopup(`<a href ='https://en.wikipedia.org/wiki/Japan'>more information</a>`).openPopup()
+
 }
 MapWrapper.prototype.addMarker = function (coords) {
   L.marker(coords).addTo(this.map);
